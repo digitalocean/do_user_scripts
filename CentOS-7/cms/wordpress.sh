dd185f3bf60b3fdf2131eff02806f7ad0b3744d6
Ryan Quinn <ryan@digitalocean.com>
2015-06-10 15:54:05 -0400

    Added Wordpress for CentOS 7 script

---
+++ CentOS-7/cms/wordpress.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+#
+# Wordpress Setup Script
+#
+# This script will install and configure Wordpress on
+# an CentOS 7 droplet
+
+# Generate root and wordpress mysql passwords
+rootmysqlpass="<%ROOT_MYSQL_PASSWORD%>";
+wpmysqlpass="<%WORDPRESS MYSQL PASSWORD%>";
+
+# Update CentOS
+yum -y update;
+
+# Install Apache/MySQL
+yum -y install httpd php php-mysql mariadb-server mariadb unzip;
+
+# Start services
+systemctl start mariadb;
+systemctl start httpd;
+
+# Download and uncompress Wordpress
+wget https://wordpress.org/latest.zip -O /tmp/wordpress.zip;
+cd /tmp/;
+unzip /tmp/wordpress.zip;
+# Set up database user
+/usr/bin/mysqladmin -u root -h localhost create wordpress;
+/usr/bin/mysqladmin -u root -h localhost password $rootmysqlpass;
+/usr/bin/mysql -uroot -p$rootmysqlpass -e "CREATE USER wordpress@localhost IDENTIFIED BY '"$wpmysqlpass"'";
+/usr/bin/mysql -uroot -p$rootmysqlpass -e "GRANT ALL PRIVILEGES ON wordpress.* TO wordpress@localhost";
+
+# Configure Wordpress
+cp /tmp/wordpress/wp-config-sample.php /tmp/wordpress/wp-config.php;
+sed -i "s/'DB_NAME', 'database_name_here'/'DB_NAME', 'wordpress'/g" /tmp/wordpress/wp-config.php;
+sed -i "s/'DB_USER', 'username_here'/'DB_USER', 'wordpress'/g" /tmp/wordpress/wp-config.php;
+sed -i "s/'DB_PASSWORD', 'password_here'/'DB_PASSWORD', '$wpmysqlpass'/g" /tmp/wordpress/wp-config.php;
+cp -Rf /tmp/wordpress/* /var/www/html/.;
+rm -f /var/www/html/index.html;
+chown -Rf apache:apache /var/www/html;
+systemctl enable httpd.service;
+systemctl enable mariadb.service;
+systemctl restart httpd.service;
