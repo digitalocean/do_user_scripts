c157947dbc8e45e39d111950476024488d11ad5e
Ryan Quinn <ryan@digitalocean.com>
2015-06-15 16:18:04 -0400

    minor fix on U-14.04 Mediawiki, Added CentOS 7 MediaWiki script

--- CentOS-7/cms/README.md
+++ CentOS-7/cms/README.md
@@ -2,3 +2,6 @@
 
 ##wordpress.sh
 This script will install and configure Wordpress.  This stack includes Apache2, PHP5, and MySQL. 
+
+## mediawiki.sh
+This script will install and configure MediaWiki with Apache, PHP, and MySQL.  Details on the database user created can be found in the MOTD shown when you log in via ssh.  After this script is complete you can browse to your new droplet's IP address to complete the setup.
--- CentOS-7/cms/mediawiki.sh
+++ CentOS-7/cms/mediawiki.sh
@@ -3,26 +3,24 @@
 # MediaWiki Setup Script
 #
 # This script will install and configure MediaWiki on
-# an Ubuntu 14.04 droplet
-export DEBIAN_FRONTEND=noninteractive;
+# a CentOS 7 droplet
 
 # Generate root and wordpress mysql passwords
 rootmysqlpass=`dd if=/dev/urandom bs=1 count=32 2>/dev/null | base64 -w 0 | rev | cut -b 2- | rev`
 mwmysqlpass=`dd if=/dev/urandom bs=1 count=32 2>/dev/null | base64 -w 0 | rev | cut -b 2- | rev`
 
 # Write passwords to file
-echo "MySQL Passwords for this droplet " > /etc/motd.tail;
-echo "-----------------------------------" >> /etc/motd.tail;
-echo "Root MySQL Password: $rootmysqlpass" >> /etc/motd.tail;
-echo "MediaWiki MySQL Database: mwdb" >> /etc/motd.tail;
-echo "Mediawiki MySQL Username: mwsql" >> /etc/motd.tail;
-echo "Mediawiki MySQL Password: $mwmysqlpass" >> /etc/motd.tail;
-echo "-----------------------------------" >> /etc/motd.tail;
-echo "You can remove this information with 'rm -f /etc/motd.tail'" >> /etc/motd.tail;
+echo "MySQL Passwords for this droplet " > /etc/motd;
+echo "-----------------------------------" >> /etc/motd;
+echo "Root MySQL Password: $rootmysqlpass" >> /etc/motd;
+echo "MediaWiki MySQL Database: mwdb" >> /etc/motd;
+echo "Mediawiki MySQL Username: mwsql" >> /etc/motd;
+echo "Mediawiki MySQL Password: $mwmysqlpass" >> /etc/motd;
+echo "-----------------------------------" >> /etc/motd;
+echo "You can remove this information with 'cat /dev/null > /etc/motd'" >> /etc/motd;
 
-apt-get update;
-apt-get -y install apache2 mysql-server libapache2-mod-auth-mysql php5-mysql php5 libapache2-mod-php5 php5-mcrypt php5-gd php5-intl php-pear php5-dev make libpcre3-dev php-apc;
-
+yum -y install httpd mariadb-server mariadb php-mysql php php-mcrypt php-gd php-xml;
+systemctl start mariadb;
 # Set up database user
 /usr/bin/mysqladmin -u root -h localhost create mwdb;
 /usr/bin/mysqladmin -u root -h localhost password $rootmysqlpass;
@@ -35,10 +33,10 @@ wget http://releases.wikimedia.org/mediawiki/1.25/mediawiki-1.25.1.tar.gz -O /ro
 cd /root;
 tar -zxf /root/mediawiki.tar.gz;
 cp -Rf /root/mediawiki-1.25.1/* /var/www/html/.;
-#rm /root/mediawiki.tar.gz;
-#rm -Rf /root/mediawiki-1.25.1;
-chown -Rf www-data.www-data /var/www/html;
-service apache2 restart;
+rm /root/mediawiki.tar.gz;
+rm -Rf /root/mediawiki-1.25.1;
+chown -Rf apache.apache /var/www/html;
+systemctl start httpd;
 
 cat /etc/motd.tail > /var/run/motd.dynamic;
 chmod 0660 /var/run/motd.dynamic;
